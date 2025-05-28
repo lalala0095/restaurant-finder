2ba276dfca3827888263b107a5be8806da880e5d
Lemuel Torrefiel <torrefiellemuel@gmail.com>
2025-05-28 08:53:22 +0800

    EDIT title and backend link

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App: React.FC = () => {
     const requestBody: ApiRequest = { message };
 
     try {
-      const response = await fetch('https://belly-compare-committed-pressed.trycloudflare.com/api/execute', {
+      const response = await fetch('https://newsletters-rochester-promptly-were.trycloudflare.com/api/execute', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
